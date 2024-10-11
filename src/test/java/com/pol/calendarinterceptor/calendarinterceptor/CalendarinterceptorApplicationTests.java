e3062fbbf6f8622b2a9a108f7923a4bdeb042d5b
Pol Gubau Amores <gubaupol@gmail.com>
2024-10-11 12:58:22 +0200

    first commit

---
+++ src/test/java/com/pol/calendarinterceptor/calendarinterceptor/CalendarinterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pol.calendarinterceptor.calendarinterceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarinterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
